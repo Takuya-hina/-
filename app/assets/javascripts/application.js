9d569be458e527169def79b7e8d60ef7a5bf8ecc
Takuya.Iwahasi <takuya.hina62@gmail.com>
2020-10-03 13:52:08 +0000

    201003全ページグリッド調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
